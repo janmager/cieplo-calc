f5cd635f2c867b1d113550f0e32b4f4e227b19bd
Jan Mager <jasiu.mager@gmail.com>
2025-04-25 11:01:21 +0200

    clean page

---
+++ app/clean/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import CalculatorContainer from '../components/Calculator/CalculatorContainer'
+
+function page() {
+  return (
+    <CalculatorContainer />
+  )
+}
+
+export default page
