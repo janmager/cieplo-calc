b0b0025da55bf99dc3853cc9843784b48db9e783
Jan Mager <jasiu.mager@gmail.com>
2025-08-10 20:15:38 +0200

    all

--- app/components/Calculator/2-screen/5-step/SecondStepView5.tsx
+++ app/components/Calculator/2-screen/5-step/SecondStepView5.tsx
@@ -86,7 +86,7 @@ function SecondStepView5({formData, setFormData, errors, setErrors, products}: {
         }
         finally{
             window.parent.postMessage({ type: 'redirectToResult', id: formData.id }, '*');
-            // router.push(`/wynik/${formData.id}`)
+            router.push(`/wynik/${formData.id}`)
         }
     }
 
